92d23a73a4fb56ccb335cd94b5f586763278840c
Your Name <tramttb20406c@st.uel.edu.vn>
2023-04-23 15:00:04 +0700

    add login/register page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -10,12 +10,33 @@ import { CustomerDetailComponent } from './customer-detail/customer-detail.compo
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { Ng2OrderModule } from 'ng2-order-pipe';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TinTucComponent } from './tin-tuc/tin-tuc.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
+import { BestSellingProductsComponent } from './best-selling-products/best-selling-products.component';
+import { BiscottiComponent } from './biscotti/biscotti.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponent,
     CustomerComponent,
     CustomerDetailComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    TinTucComponent,
+    BestSellerComponent,
+    BestSellingProductsComponent,
+    BiscottiComponent,
+    CarouselComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
